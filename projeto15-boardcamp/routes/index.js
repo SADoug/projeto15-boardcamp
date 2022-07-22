d4f440f74a3705705ffb389368b495aee484aa28
SADoug <douglastec2@gmail.com>
2022-07-22 17:15:57 -0300

    Rentals CRUD done

--- projeto15-boardcamp/routes/index.js
+++ projeto15-boardcamp/routes/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import categoryRouter from "./categoryRouter.js";
 import gamesRouter from "./gamesRouter.js";
 import customersRouter from "./customersRouter.js";
+import rentalsRouter from "./rentalsRouter.js";
 
 const router = Router();
 router.use(categoryRouter);
 router.use(gamesRouter); 
 router.use(customersRouter); 
+router.use(rentalsRouter); 
 
 export default router;
