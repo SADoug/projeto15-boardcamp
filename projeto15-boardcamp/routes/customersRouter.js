3f40f3e9e5b236f1cfd7faa416dd2cfbe7591d2c
SADoug <douglastec2@gmail.com>
2022-07-22 17:04:35 -0300

    Customers CRUD done

---
+++ projeto15-boardcamp/routes/customersRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createCustomer, getCustomer, getCustomers, updateCustomer } from "../controllers/customerController.js";
+import { validateCustomer } from "../middlewares/customerValidator.js";
+
+const customersRouter = Router();
+
+customersRouter.get("/customers", getCustomers);
+customersRouter.get("/customers/:id", getCustomer);
+customersRouter.post("/customers", validateCustomer, createCustomer);
+customersRouter.put("/customers/:id", validateCustomer, updateCustomer);
+
+export default customersRouter;
--- projeto15-boardcamp/routes/index.js
+++ projeto15-boardcamp/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import categoryRouter from "./categoryRouter.js";
 import gamesRouter from "./gamesRouter.js";
+import customersRouter from "./customersRouter.js";
 
 const router = Router();
 router.use(categoryRouter);
 router.use(gamesRouter); 
+router.use(customersRouter); 
 
 export default router;
